67376a2cdf7c1279c3c2c02f9445142a60674c9c
Oluwatimi11 <masterarcher6321@gmail.com>
2022-04-03 01:41:52 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,125 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/dWMyZJq/pexels-ron-lach-8386668.jpg",
+        // img: "../public/img/img1.jpg",
+        title: "SUMMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.", 
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/t3ryLzP/pexels-hassan-ouajbir-1860160.jpg",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/hDSr8tb/pexels-mentatdgt-1676102.jpg",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.", 
+        bg: "fbf0f4",
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/wYjf5SX/pexels-marlon-schmeiski-6747302.jpg",
+        title: "SHIRT STYLE",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/7NjQKzv/pexels-mentatdgt-1748760.jpg",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/J5dL558/pexels-cottonbro-6862367.jpg",
+        title: "LIGHT JACKETS",
+    },
+]
+
+
+
+export const popularProducts = [
+{
+    id:1,
+    img:"https://i.ibb.co/8gQS8SL/pexels-skylar-kang-6046231.jpg",
+    // img:"https://i.ibb.co/VVx4Bck/pexels-a-ng-thanh-tu-5693888.jpg",
+},
+{
+    id:2,
+    img:"https://i.ibb.co/LYB6tGL/pexels-daria-shevtsova-1030946.jpg",
+},
+{
+    id:3,
+    img: "https://i.ibb.co/hRTP9mY/pexels-craig-adderley-1498020.jpg",
+},
+{
+    id:4,
+    img:"https://i.ibb.co/gFn7Kqh/pexels-liza-summer-6347892.jpg",
+},
+{
+    id:5,
+    img:"https://i.ibb.co/g494txm/pexels-everton-nobrega-4142539.jpg",
+},
+{
+    id:6,
+    img:"https://i.ibb.co/CbzF44S/pexels-melvin-buezo-2529148.jpg",
+},
+{
+    id:7,
+    img:"https://i.ibb.co/vd8c1JL/pexels-pixabay-47037.jpg",
+},{
+    id:8,
+    img:"https://i.ibb.co/yQpVzyR/pexels-lilartsy-1159670.jpg",
+},
+{
+    id:9,
+    img:"https://i.ibb.co/Tr2kQmW/pexels-luis-quintero-3731256.jpg",
+},
+{
+    id:10,
+    img:"https://i.ibb.co/Sf7LJcj/pexels-pixabay-52518.jpg",
+},{
+    id:11,
+    img:"https://i.ibb.co/fr6xDmW/pexels-karolina-grabowska-4210866.jpg",
+},
+{
+    id:12,
+    img:"https://i.ibb.co/Fzx6v0Y/pexels-evg-kowalievska-1040384.jpg",
+},
+{
+    id:13,
+    img:"https://i.ibb.co/4fGH0Vj/pexels-harrison-haines-3536235.jpg",
+},{
+    id:14,
+    img:"https://i.ibb.co/nBgw694/pexels-ichad-windhiagiri-5432532.jpg",
+},
+{
+    id:15,
+    img:"https://i.ibb.co/7tsr5Gw/pexels-greg-contreras-3177812.jpg",
+},
+{
+    id:16,
+    img:"https://i.ibb.co/0mksqV6/pexels-bogdan-glisik-1661471.jpg",
+},{
+    id:17,
+    img:"https://i.ibb.co/XSzm88S/pexels-elegance-nairobi-3389419.jpg",
+},
+{
+    id:18,
+    img:"https://i.ibb.co/0BTV3wb/pexels-matheus-bertelli-2905238.jpg",
+},
+{
+    id:19,
+    img:"https://i.ibb.co/sJZn7Mb/pexels-ron-lach-9595079.jpg",
+},{
+    id:20,
+    img:"https://i.ibb.co/Vv983vV/pexels-aman-jakhar-2048548.jpg",
+},
+
+]
+
